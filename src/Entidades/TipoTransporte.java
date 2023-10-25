76239773482d1def0972c146eb8c1b4570c23f35
maurobenito <maurogbenito@gmail.com>
2023-10-25 11:26:05 -0300

    Update TipoTransporte.java

--- src/Entidades/TipoTransporte.java
+++ src/Entidades/TipoTransporte.java
@@ -9,11 +9,29 @@ package Entidades;
  * @author franc
  */
 public enum TipoTransporte {
+<<<<<<< Updated upstream
     AvionNacional(100000),
     AvionInternacional(300000),
     Tren(50000),
     Autobus(70000);
 
+=======
+    AvionaCordoba(100000),
+    TrenaCordoba(50000),
+    AutobusaCordoba(70000),
+    AvionaMendoza(150000),
+    TrenaMendoza(75000),
+    AutobusaMendoza(90000),
+    AvionaSalta(120000),
+    TrenaaSalta(65000),
+    AutobusaSalta(85000),
+    AvionaFloripa(200000),
+    AutobusaFloripa(12000),
+    AvionaSanLuis(50000),
+    TrenaSanLuis(9000),
+    AutobusaSanLuis(19000),
+    AvionaMiami(430000);
+>>>>>>> Stashed changes
     private final double importe;
 
     TipoTransporte(double importe) {
